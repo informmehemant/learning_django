aabffc33ae2496643a6a15d2b5e7fc44d7e9a764
Hemant singh <info90.hemant@gmail.com>
2020-12-29 22:49:45 +0530

    Modules: login system integrated

--- api/settings.py
+++ api/settings.py
@@ -52,7 +52,16 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'api.urls'
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
 
+    ]
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
