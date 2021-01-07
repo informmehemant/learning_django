06035161ecb66b508f59b6962c61c756afc2123d
Hemant singh <info90.hemant@gmail.com>
2021-01-07 17:23:39 +0530

    class based view added

--- api/settings.py
+++ api/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.AllowAny',
 
     ]
 }
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from snippets import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('snippets/<int:pk>', views.snippet_detail),
-    path('snippets/', views.snippet_list)
+    path('snippets/<int:pk>', views.SnippetDetails.as_view()),
+    path('snippets/', views.SnippetList.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>', views.UserDetail.as_view())
+
 ]
+urlpatters = format_suffix_patterns(urlpatterns)
